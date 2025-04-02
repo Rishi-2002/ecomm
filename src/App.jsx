45eee2d7122c7b2e53730380e3869d5efe483e47
Rishi-2002 <rishi.mic121@gmail.com>
2025-04-02 15:08:40 +0530

    added colon

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import {BrowserRouter,Route,Routes} from 'react-router-dom'
 import Home from './pages/Home'
 import Navbar from '../components/Navbar'
-import Cart from './pages/Cart'
+import Cart from './pages/Cart';
 const App = () => {
   return (
     <div>
