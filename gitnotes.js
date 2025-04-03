b6e50d6c6010875c121c3fa508254f1cca2ad64c
Rishi-2002 <rishi.mic121@gmail.com>
2025-04-03 14:53:39 +0530

    added vercel json file

--- gitnotes.js
+++ gitnotes.js
@@ -3,3 +3,10 @@
 // 3-->git config user.email "github email"
 // 4>git add .
 // 5>>
+
+
+// clone-->
+// git clone (github repositary link)------in terminal
+// go in project 1)cd
+				//  2)npm i(for installing package json)
+				// 3) git pull-->(to get updated code of the github user we cloned into our vs code)
